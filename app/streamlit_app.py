7c269de26f179fd74d69693b7fe0a4b6b1182477
Your Name <you@example.com>
2026-08-26 07:03:54 +0800

    Fix title

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -177,7 +177,7 @@ p, label {
 # TITLE
 # ============================================================
 
-st.title("🌱 Crop Disease Detection - Final Year Project")
+st.title("🌱 Crop Disease Detection System- Final Year Project")
 st.caption(
     "Upload a crop leaf image and receive disease detection, "
     "expected symptoms, and treatment recommendations."
